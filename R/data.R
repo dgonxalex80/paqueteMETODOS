27f8f78dbc1366aef1b3bb1ea6f251fa84d7d183
Daniel E. Gonzalez <dgonxalex80@gmail.com>
2024-02-01 09:48:30 -0500

    ajustes descricion datasets

--- R/data.R
+++ R/data.R
@@ -38,21 +38,71 @@
 "ausentismo"
 
 
-#' Cervezas
+#' CarreraLuz22
 #'
 #' Esta data recoge algunas de las caracteristicas de la cerveza
-#' @format Data compuesta por 69 marcas de cerveza y 6 variables
+#' @format Data compuesta por 1922  atletas y 6 7 variables
 #' \describe{
-#'  \item{marca}{marca de la cerveza}
-#'  \item{precio}{precio de presentación de 6 botellas de 12 onzas}
-#'  \item{calorias}{cantidad de caloroas por unidad de 12 onzas}
-#'  \item{poralcoh}{porcentaje de alcohol por 12 onzas de contenido}
-#'  \item{tipo}{1 lager artesanal, 2 clara artesanal, 3 lager importada, 4 cerveza normal y helada, y 5 cerveza baja en calorías y sin alcohol}
-#'  \item{origen}{1 nacional, 2 importada}
+#'  \item{id}{identificador del atleta}
+#'  \item{sex}{sexo del atleta - "Hombre" , "Mujer"}
+#'  \item{edad}{edad del atleta en años}
+#'  \item{origen}{ciudad de origen del atleta}
+#'  \item{timerun}{tiempo de carrera en segundos}
+#'  \item{categoria}{categoria del atleta: "1. Juvenil", "2. Abierta", "3. Veteranos A", "4. Veteranos B", "5. Veteranos C" }
+#'  \item{nacionalidad}{nacionalidad del atleta participante : ARG, BTN, COL, DEU, VEN}
 #' }
 #' @examples
-#' beer
-"beer"
+#' CarreraLuz22
+"CarreraLuz22"
+
+
+#' Rotacion
+#'
+#' Esta data recoge algunas de las caracteristicas de empleados de una empresa
+#' @format Data compuesta por 1470  empleados y 25 variables
+#' \describe{
+#'  \item{id}{identificador del empleado}
+#'  \item{Rotacion}{rotacion del empleado : Si, No}
+#'  \item{Edad}{edad del empleado en años}
+#'  \item{Viaje de negocio}{frecuencia de los viajes}
+#'  \item{Departamento}{departamento al que pertenece el empleado}
+#'  \item{Distancia_Casa}{distancia en kilometros casa empresa}
+#'  \item{Educacion}{nivel educativo del empleado}
+#'  \item{Campo_Educacion}{area de educacion}
+#'  \item{Satisfación_Ambiental}{evaluacion perseccion ambiental}
+#'  \item{Genero}{genero del empleado: F, M}
+#'  \item{Cargo}{cargo del empleado}
+#'  \item{Satisfación_Laboral}{nivel de satisfaccion en el trabajo}
+#'  \item{Estado_Civil}{estado civil del empleado}
+#'  \item{Ingreso_Mensual}{Ingresos mensuales del empleado}
+#'  \item{Trabajos_Anteriores}{Numero de trabajos previos}
+#'  \item{Horas_Extra}{trabaja horas extras : Si, No}
+#'  \item{Porcentaje_aumento_Laboral}{incremento  salanrio}
+#'  \item{Rendimiento_Laboral}{evaluacion rendimiento laboral}
+#'  \item{Años_Experiencia}{años de experiencia profesional}
+#'  \item{Capacitaciones}{numero de capacitaciones}
+#'  \item{Equilibio_Trabajo_Vida}{valoracion equilibrio casa-trabajo}
+#'  \item{Antigüedad}{años de trabajo en la empresa}
+#'  \item{Antigüedad_Cargo}{años trabajando en cargo actual}
+#'  \item{Años_ultima_promoción}{años desde la ultima promocion}
+#'  \item{Años_acargo_con_mismo_jefe}{años con el ultimo jefe}
+#' }
+#' @examples
+#' rotacionNA
+"rotacionNA"
 
 
 
+
+#' seriesCOL
+#'
+#' Esta data recoge las series inflacion y tasa de cambio USD$
+#' @format Data compuesta por 74  atletas y 3 variables
+#' \describe{
+#'  \item{year}{año}
+#'  \item{INF}{tasa de inflacion}
+#'  \item{TC}{tasa de cambio}
+#' }
+#' @examples
+#' seriesCOL
+"seriesCOL"
